570e2f0a3102df03d42f71ffb1a2eaedbebf5f77
treywood10 <totreywood@aol.com>
2023-09-14 17:09:14 -0400

    Updated models

--- BTB_analysis.py
+++ BTB_analysis.py
@@ -356,6 +356,10 @@ train_compare = pd.concat([train_compare,
                           ignore_index = True).sort_values('Test_F1', ascending = False)
 
 
+#
+#### Random Forest
+#
+
 # Objective function for random forest #
 def rf_tuning(X_train, y_train, pbounds, n_init, n_iter, seed):
     def obj_forest(n_estimators, criterion,
@@ -485,3 +489,82 @@ train_compare = pd.concat([train_compare,
                             'Test_F1': test_f1,
                             'hypers': [best_model]})], 
                           ignore_index = True).sort_values('Test_F1', ascending = False)
+
+#
+#### XGBoost
+#
+
+# Objective function for XGBoost #
+def boost_tuning(X_train, y_train, pbounds, n_init, n_iter, seed):
+    def obj_xgb(n_estimators, max_depth, 
+                learning_rate, subsample, 
+                colsample_bytree, 
+                reg_alpha, reg_lambda):
+        """
+        Function for bayesian search of best hyperparameters. 
+
+        Parameters
+        ----------
+        n_estimators : int
+            Number of boosting rounds.
+        max_depth : int
+            Max tree depth for base learners.
+        learning_rate : float
+            Boosting learning rate.
+        subsample : float
+            Subsample ratio of training data.
+        colsample_bytree : float
+            Subsample ratio of columns used.
+        reg_alpha : float
+            L1 regularization.
+        reg_lambda : float
+            L2 regularization.
+
+        Returns
+        -------
+        f_score : float
+            F1 score to measure model performance.
+        
+        """
+    
+
+        # Instantiate modlel #
+        model = XGBClassifier(n_estimators = int(n_estimators),
+                                       max_depth = int(max_depth),
+                                       learning_rate = learning_rate,
+                                       subsample = subsample,
+                                       colsample_bytree = colsample_bytree,
+                                       reg_alpha = reg_alpha,
+                                       reg_lambda = reg_lambda,
+                                       random_state = seed,
+                                       n_jobs = 2)
+    
+        # Cross validation #
+        pred = cross_val_predict(model, X_train, y_train, cv = 5)
+    
+        # F1 Score #
+        f_score = f1_score(y_train, pred)
+        
+        return f_score
+
+    # Set optimizer #
+    optimizer = BayesianOptimization(f = obj_xgb, pbounds = pbounds,
+                                     random_state = seed)
+
+
+    # Call maximizer #
+    optimizer.maximize(init_points = n_init, n_iter = n_iter)
+    
+    # Pull best info #
+    best_hypers = optimizer.max['params']
+    best_f1 = optimizer.max['target']
+
+
+    # Adjust hyperparamters #
+    best_hypers['n_estimators'] = round(best_hypers['n_estimators'])
+
+    best_hypers['max_depth'] = round(best_hypers['max_depth'])
+
+    best_model = XGBClassifier(**best_hypers)
+    
+    return best_f1, best_model 
